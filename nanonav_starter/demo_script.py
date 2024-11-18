1fa16dbc9f17675a9b737e5229c28c4a0e1aeaff
SathR12 <sathya_regu@outlook.com>
2024-11-18 17:04:16 -0500

    fixed error on line 19 to time.sleep(2)

--- nanonav_starter/demo_script.py
+++ nanonav_starter/demo_script.py
@@ -9,14 +9,14 @@ robot.m1_forward(30)
 robot.m2_forward(30)
 
 # Printing encoder values
-print("Encoder 1 Value:", robot.get_enc1())
+print("\nEncoder 1 Value:", robot.get_enc1())
 print("Encoder 2 Value:", robot.get_enc2())
 
 time.sleep(2)
 
 # Stop
 robot.stop()
-robot.sleep(2)
+time.sleep(2)
 
 # Move backward for 2 seconds
 robot.m1_backward(30)
