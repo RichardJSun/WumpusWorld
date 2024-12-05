1b0ca2b2e1969d190582ea6a00ac22f720158903
SathR12 <sathya_regu@outlook.com>
2024-12-05 16:35:07 -0500

    Added BLE functionality

--- nanonav_starter/robot_movements.py
+++ nanonav_starter/robot_movements.py
@@ -1,12 +1,24 @@
 #  robot_movements.py By: sathya - Mon Nov 25 2024
 
 from nanonav import NanoBot
-import time
+from nanonav import BLE
 import math
+import time
 
-# Custom python file imports
-import constants
+class GridConstants:
+    # Measured in cm
+    TILE_SIZE = 38.1
 
+class RobotConstants:
+    # Measured in cm
+    WHEEL_DIAMETER = 4.08
+    WHEELBASE = 12
+
+    # Measured in encoder ticks
+    COUNTS_PER_ROTATION = 181
+
+    # Measured in percentage
+    SPEED = 30
 
 class Robot:
 
@@ -23,8 +35,9 @@ class Robot:
         # Initializing and resetting encoders
         self.enc1 = self.robot.get_enc1()
         self.enc2 = self.robot.get_enc2()
-        self.enc1.set_enc1(0)
-        self.enc2.set_enc2(0)
+
+        self.robot.set_enc1(0)
+        self.robot.set_enc2(0)
 
         self.speed = speed
         self.counts_per_rotation = counts_per_rotation
@@ -37,72 +50,68 @@ class Robot:
         print("### COMPLETE INITIALIZING ROBOT ###")
 
 
-    def moveForward(self, squares):
+    def moveForward(self):
 
         # Iterates until the number of squares argument
-        for i in range(squares):
+        # Resetting infrared sensors
+        self.left_ir = self.robot.ir_left()
+        self.right_ir = self.robot.ir_right()
 
-            # Resetting infrared sensors
+        # Iterates until white color is detected
+        # Note: ir returns true if white is detected
+        while not self.left_ir and not self.right_ir:
+
+            if self.debug:
+                print("DEBUG: IR sensor only detected black so far")
+
+            # Motors spin to move directly forward
+            self.robot.m1_forward(self.speed)
+            self.robot.m2_forward(self.speed)
+
+            # Fetch ir sensor values to update throughout iteration
             self.left_ir = self.robot.ir_left()
             self.right_ir = self.robot.ir_right()
 
-            # Iterates until white color is detected
-            # Note: ir returns true if white is detected
-            while not self.left_ir and not self.right_ir:
+            time.sleep(0.1)
 
-                if self.debug:
-                    print("DEBUG: IR sensor only detected black so far")
+        if self.debug:
+            print("DEBUG: IR sensors detected white.")
 
-                # Motors spin to move directly forward
-                self.robot.m1_forward(self.speed)
-                self.robot.m2_forward(self.speed)
+        # After white is detected, robot should move to the middle of the tile
+        # Setting forward argument as true to move forwards to the middle of the previous square
+        self.moveToMiddle(forward = True)
+        self.robot.stop()
 
-                # Fetch ir sensor values to update throughout iteration
-                self.left_ir = self.robot.ir_left()
-                self.right_ir = self.robot.ir_right()
-
-                time.sleep(0.1)
-
-            if self.debug:
-                print("DEBUG: IR sensors detected white.")
-
-            # After white is detected, robot should move to the middle of the tile
-            # Setting forward argument as true to move forwards to the middle of the previous square
-            self.moveToMiddle(forward = True)
-            self.robot.stop()
-
-    def moveBackward(self, squares):
+    def moveBackward(self):
 
         # Iterates until the number of squares argument
-        for i in range(squares):
+        # Resetting infrared sensors
+        self.left_ir = self.robot.ir_left()
+        self.right_ir = self.robot.ir_right()
 
-            # Resetting infrared sensors
-            self.left_ir = self.robot.ir_left()
-            self.right_ir = self.robot.ir_right()
-
-            # Iterates until white color is detected
-            # Note: ir returns true if white is detected
-            while not self.left_ir and not self.right_ir:
-
-                if self.debug:
-                    print("DEBUG: IR sensor only detected black so far")
-
-                # Motors spin to move directly backward
-                self.robot.m1_backward(self.speed)
-                self.robot.m2_backward(self.speed)
-
-                # Fetch ir sensor values to update throughout iteration
-                self.left_ir = self.robot.ir_left()
-                self.right_ir = self.robot.ir_right()
-                time.sleep(0.1)
+        # Iterates until white color is detected
+        # Note: ir returns true if white is detected
+        while not self.left_ir and not self.right_ir:
 
             if self.debug:
-                print("DEBUG: IR sensors detected white.")
+                print("DEBUG: IR sensor only detected black so far")
 
-            # After white is detected, robot should move to the middle of the tile
-            # Setting forward argument as false to move backwards to the middle of the previous square
-            self.moveToMiddle(forward = False)
-            self.robot.stop()
+            # Motors spin to move directly backward
+            self.robot.m1_backward(self.speed)
+            self.robot.m2_backward(self.speed)
+
+            # Fetch ir sensor values to update throughout iteration
+            self.left_ir = self.robot.ir_left()
+            self.right_ir = self.robot.ir_right()
+            time.sleep(0.1)
+
+        if self.debug:
+            print("DEBUG: IR sensors detected white.")
+
+        # After white is detected, robot should move to the middle of the tile
+        # Setting forward argument as false to move backwards to the middle of the previous square
+        self.moveToMiddle(forward = False)
+        self.robot.stop()
 
     def moveLeft(self, rotations = 1):
 
@@ -125,8 +134,8 @@ class Robot:
         while abs(self.robot.get_enc1()) < target_encoder_count and abs(self.robot.get_enc2()) < target_encoder_count:
 
            # Sets motor 1 to move the opposite direction as motor 2
-           self.robot.m1_backward(self.speed)
-           self.robot.m2_forward(self.speed)
+           self.robot.m1_forward(self.speed)
+           self.robot.m2_backward(self.speed)
 
            time.sleep(0.1)
 
@@ -141,7 +150,8 @@ class Robot:
         # Calculating the number of rotations to achieve the turn distance
         wheel_circumference = math.pi * self.wheel_diameter
         turn_rotations = turn_distance / wheel_circumference
-        target_encoder_count = turn_rotations * self.counts_per_rotation
+        # Note: hardcoded the turning encoder count to 10 for right motor as its different from left
+        target_encoder_count = turn_rotations * (self.counts_per_rotation + 10)
 
         # Resetting encoders to prepare for turning
         self.robot.set_enc1(0)
@@ -153,8 +163,8 @@ class Robot:
         while abs(self.robot.get_enc1()) < target_encoder_count and abs(self.robot.get_enc2()) < target_encoder_count:
 
            # Sets motor 1 to move the opposite direction as motor 2
-           self.robot.m1_forward(self.speed)
-           self.robot.m2_backward(self.speed)
+           self.robot.m1_backward(self.speed)
+           self.robot.m2_forward(self.speed)
 
            time.sleep(0.1)
 
@@ -194,7 +204,11 @@ class Robot:
 
         self.robot.stop()
 
-    def calibrateEncoders(self, duration = 2):
+    # Needed for "pending" state
+    def stop(self):
+        self.robot.stop()
+
+    def calibrateEncoders(self):
 
         print("### STARTING ENCODER CALIBRATION ###")
 
@@ -202,7 +216,7 @@ class Robot:
         self.robot.set_enc1(0)
         self.robot.set_enc2(0)
 
-        print("Rotate the wheel")
+        print("Rotate the wheel...")
 
         while True:
 
@@ -214,18 +228,53 @@ class Robot:
             average = (enc1_count + enc2_count) // 2
             print(f"Enc1: {enc1_count}, Enc2: {enc2_count}, Avg: {average}")
 
+class RobotBLE(BLE):
+    def on_connected(self):
+        print("Connection established with NanoNav")
+
+    def on_disconnected(self):
+        print("Disconnected from NanoNav")
+
 
 if __name__ == "__main__":
-    wumpusBot = Robot(
-        speed = constants.RobotConstants.SPEED,
-        counts_per_rotation = constants.RobotConstants.COUNTS_PER_ROTATION,
-        wheel_diameter = constants.RobotConstants.WHEEL_DIAMETER,
-        wheel_base = constants.RobotConstants.WHEELBASE,
-        tile_size = constants.GridConstants.TILE_SIZE,
+    robot = Robot(
+        speed = RobotConstants.SPEED,
+        counts_per_rotation = RobotConstants.COUNTS_PER_ROTATION,
+        wheel_diameter = RobotConstants.WHEEL_DIAMETER,
+        wheelbase = RobotConstants.WHEELBASE,
+        tile_size = GridConstants.TILE_SIZE,
         debug = True
     )
 
-    wumpusBot.calibrateEncoders()
+    # BLE based commands per signals
+    ble_commands = {
+        0 : robot.stop,
+        1 : robot.moveForward,
+        2 : robot.moveBackward,
+        3 : robot.moveLeft,
+        4 : robot.moveRight
+    }
+
+    ble = RobotBLE(name = "NanoNav")
+    ble.send(0);
+    response = ble.read()
+    while True:
+        response = ble.read()
+        time.sleep(0.5)
+        if response:
+            print(f"Response received: {response}")
+
+            if response in ble_commands:
+                ble_commands[response]()
+                ble.send(0)
+
+        time.sleep(0.5)
+
+
+
+
+
+
 
 
 
